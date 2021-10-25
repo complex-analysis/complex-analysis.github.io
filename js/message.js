485ba69eadc10327890776da0996579e49dda5b3
Juan Carlos Ponce Campuzano <37394697+jcponce@users.noreply.github.com>
2021-10-25 20:47:52 +1000

    support message. Thanks

---
+++ js/message.js
@@ -0,0 +1,37 @@
+// Get the modal
+let modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+let btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+//btn.onclick = function() {
+//  modal.style.display = "block";
+//}
+
+function showMessage(){
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+  clearInterval(myInterval);
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    clearInterval(myInterval);
+  }
+  
+}
+
+let myInterval = setInterval(function () {
+    showMessage ();
+}, 8000);
+
