6c343339eb35e170ecbc96654de9ae283fb72bdb
Juan Carlos Ponce Campuzano <37394697+jcponce@users.noreply.github.com>
2023-12-29 08:57:40 +1000

    new

---
+++ applets/p5js/domain-coloring/fullscreen/sketch.js
@@ -0,0 +1,19 @@
+/* 
+Written in p5.js (https://p5js.org/)
+Under Creative Commons License
+https://creativecommons.org/licenses/by-sa/4.0/
+Written by Juan Carlos Ponce Campuzano, 16-Oct-2020
+https://jcponce/github/io
+
+Notes: To be refactored. :)
+*/
+
+function setup(){
+    
+    let canvas = createCanvas(500, 500);
+      canvas.parent('canvasContainer');
+}
+
+function draw(){
+    background(100);
+}
