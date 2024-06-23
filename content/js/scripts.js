b4486ea873ed99ea574b7f36994f947650703015
Juan Carlos Ponce Campuzano <37394697+jcponce@users.noreply.github.com>
2024-06-24 09:19:16 +1000

    ok

--- content/js/scripts.js
+++ content/js/scripts.js
@@ -1,1 +1,341 @@
-const darkModeToggle=document.getElementById("dark-mode-toggle");darkModeToggle.addEventListener("click",()=>{document.body.classList.toggle("latex-dark")});let darkModeIcon=document.getElementById("dark-mode-toggle");function toggleMode(){var e=document.getElementById("dark-mode-toggle");'<i class="fa-solid fa-moon"></i>'===e.innerHTML.trim()?e.innerHTML='<i class="fa-regular fa-sun"></i>':e.innerHTML='<i class="fa-solid fa-moon"></i>'}function scrollToElementWithOffset(e,n){var e=document.querySelector(e);e&&(e=e.getBoundingClientRect().top+window.scrollY-n,window.scrollTo({top:e,behavior:"smooth"}))}window.matchMedia&&window.matchMedia("(prefers-color-scheme: dark)").matches?(document.body.classList.add("latex-dark"),darkModeIcon.innerHTML='<i class="fa-solid fa-moon"></i>'):darkModeIcon.innerHTML='<i class="fa-regular fa-sun"></i>',document.getElementById("dark-mode-toggle").addEventListener("click",toggleMode);var links=document.querySelectorAll('a[href^="#"]');links.forEach(function(e){e.addEventListener("click",function(e){e.preventDefault(),scrollToElementWithOffset(this.getAttribute("href"),50)})});let mybutton=document.getElementById("toTop");function scrollFunction(){mybutton.style.display="none",(500<document.body.scrollTop||500<document.documentElement.scrollTop)&&(mybutton.style.display="block")}function topFunction(){document.querySelectorAll(".toc-chapter a").forEach(e=>{e.classList.remove("highlight")}),document.body.scrollTop=0,document.documentElement.scrollTop=0}window.onscroll=function(){scrollFunction()};const leftLink=document.getElementById("prev-link"),rightLink=document.getElementById("next-link"),centerLink=document.getElementById("center-link");function handleLinkHover(e,n,t){n=centerLink.querySelector(n),t=centerLink.querySelector(t);n&&t&&("mouseenter"===e.type?(n.style.display="none",t.style.display="inline"):"mouseleave"===e.type&&(n.style.display="inline",t.style.display="none"))}leftLink&&(leftLink.addEventListener("mouseenter",e=>{handleLinkHover(e,".content-original",".content-hover-previous")}),leftLink.addEventListener("mouseleave",e=>{handleLinkHover(e,".content-original",".content-hover-previous")})),rightLink&&(rightLink.addEventListener("mouseenter",e=>{handleLinkHover(e,".content-original",".content-hover-next")}),rightLink.addEventListener("mouseleave",e=>{handleLinkHover(e,".content-original",".content-hover-next")})),centerLink&&(centerLink.addEventListener("mouseenter",e=>{handleLinkHover(e,".content-original",".content-hover")}),centerLink.addEventListener("mouseleave",e=>{handleLinkHover(e,".content-original",".content-hover")})),window.addEventListener("scroll",function(){var e=document.querySelectorAll('div[id^="section"]');let o=document.querySelectorAll(".toc-chapter a"),l=window.scrollY;e.forEach((e,n)=>{var t=e.offsetTop-100,e=t+e.clientHeight;l>=t&&l<e&&(o.forEach(e=>{e.classList.remove("highlight")}),o[n].classList.add("highlight"))})});let modal=document.getElementById("myModal"),span=document.getElementsByClassName("close")[0];function showMessage(){modal.style.display="block"}span.onclick=function(){modal.style.display="none",clearInterval(myInterval)},window.onclick=function(e){e.target==modal&&(modal.style.display="none",clearInterval(myInterval))};let myInterval=setInterval(function(){showMessage()},18e5);var iframeContainers=document.querySelectorAll(".iframeContainer");function includeHTML(){let e,n,t,o,l;for(e=document.getElementsByTagName("*"),n=0;n<e.length;n++)if(o=(t=e[n]).getAttribute("w3-include-html"))return(l=new XMLHttpRequest).onreadystatechange=function(){4==this.readyState&&(200==this.status&&(t.innerHTML=this.responseText),404==this.status&&(t.innerHTML="Page not found."),t.removeAttribute("w3-include-html"),includeHTML())},l.open("GET",o,!0),void l.send()}iframeContainers.forEach(function(e){e.addEventListener("mouseenter",function(){document.body.style.overflow="hidden"}),e.addEventListener("mouseleave",function(){document.body.style.overflow="auto"})}),includeHTML();
+/*!
+ * Interaction for online textbook.
+ * by Juan Carlos Ponce Campuzano 7/Feb/2024
+ * Updated: 22/Feb/2024
+ * License: CC BY-NC-SA 4.0 DEED
+ * Attribution-NonCommercial-ShareAlike 4.0 International
+ * https://creativecommons.org/licenses/by-nc-sa/4.0/?ref=chooser-v1
+*/
+
+/*
+ Content:
+ * - Dark mode
+ * - Modal
+ * - Include HTML
+ * - Scroll up to "id" tags for hyperlinks
+ * - To top button
+ * - Navigation bar interaction
+*/
+
+/*---------------------------------------------------------------*/
+/* Dark mode function */
+const darkModeToggle = document.getElementById('dark-mode-toggle')
+darkModeToggle.addEventListener('click', () => {
+  document.body.classList.toggle('latex-dark');
+});
+
+/*
+let darkModeIcon = document.getElementById("dark-mode-toggle");
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  document.body.classList.add('latex-dark');
+  darkModeIcon.innerHTML = '<i class="fa-solid fa-moon"></i>';
+} else {
+  darkModeIcon.innerHTML = '<i class="fa-regular fa-sun"></i>';
+}*/
+
+// Function to replace text for Dark mode
+function toggleMode() {
+  // Get the element containing the text
+  var element = document.getElementById("dark-mode-toggle");
+
+  // Toggle between "text01" and "text02"
+  if (element.innerHTML.trim() === '<i class="fa-solid fa-moon"></i>') {
+    element.innerHTML = '<i class="fa-regular fa-sun"></i>';
+    localStorage.setItem('mode', 'light');
+  } else {
+    element.innerHTML = '<i class="fa-solid fa-moon"></i>';
+    localStorage.setItem('mode', 'dark');
+  }
+}
+
+// Function to load mode from local storage
+function loadMode() {
+  var element = document.getElementById("dark-mode-toggle");
+  var mode = localStorage.getItem('mode');
+
+  if (mode === 'light') {
+    element.innerHTML = '<i class="fa-regular fa-sun"></i>';
+  } else if (mode == 'dark') {
+    element.innerHTML = '<i class="fa-solid fa-moon"></i>';
+    document.body.classList.add('latex-dark');
+  } else {
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      document.body.classList.add('latex-dark');
+      element.innerHTML = '<i class="fa-solid fa-moon"></i>';
+    } else {
+      element.innerHTML = '<i class="fa-regular fa-sun"></i>';
+    }
+  }
+}
+
+// Add event listener for mouse click
+document.getElementById("dark-mode-toggle").addEventListener("click", toggleMode);
+
+// Load the mode when the page loads
+window.onload = loadMode;
+
+/*---------------------------------------------------------------*/
+
+
+/*---------------------------------------------------------------*/
+/* The following is to scroll up to the "id" tag for hyperlinks */
+// Function to handle smooth scrolling with offset
+
+function scrollToElementWithOffset(selector, offset) {
+  var target = document.querySelector(selector);
+  if (target) {
+    var targetPosition = target.getBoundingClientRect().top + window.scrollY - offset; // pageYOffset is deprecated :(
+    window.scrollTo({
+      top: targetPosition,
+      behavior: 'smooth'
+    });
+  }
+}
+
+// Adding event listeners to all links with the class 'scroll-link'
+var links = document.querySelectorAll('a[href^="#"]');
+links.forEach(function (link) {
+  link.addEventListener('click', function (event) {
+    event.preventDefault();
+    var targetId = this.getAttribute('href');
+    scrollToElementWithOffset(targetId, 50); // 50px offset, I want it below the navbar
+  });
+});
+
+/*---------------------------------------------------------------*/
+
+
+/*---------------------------------------------------------------*/
+/* Button to the top */
+// Get the button
+
+let mybutton = document.getElementById("toTop");
+//let prevScrollpos = window.scrollY;
+
+window.onscroll = function () { scrollFunction() };
+
+// When the user scrolls down 500px from the top of the document, show the button
+function scrollFunction() {
+  //let currentScrollPos = window.scrollY;
+  //if (prevScrollpos > currentScrollPos) {
+  mybutton.style.display = "none";
+  //} else {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    mybutton.style.display = "block";
+  }
+  //}
+  //prevScrollpos = currentScrollPos;
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  let tocLinks = document.querySelectorAll('.toc-chapter a');
+  tocLinks.forEach((link) => {
+    //link.classList.remove('active');
+    link.classList.remove('highlight');
+  });
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+/*---------------------------------------------------------------*/
+
+
+/*---------------------------------------------------------------*/
+/* Interaction with the fixed navbar on the top */
+/* left-link right-link */
+const leftLink = document.getElementById('prev-link');
+const rightLink = document.getElementById('next-link');
+const centerLink = document.getElementById('center-link');
+
+// Function to handle hover events for links
+function handleLinkHover(event, originalSelector, hoverSelector) {
+  // Find the original and hover content elements
+  const contentOriginal = centerLink.querySelector(originalSelector);
+  const contentHover = centerLink.querySelector(hoverSelector);
+
+  // Check if the content elements exist before manipulating them
+  if (contentOriginal && contentHover) {
+    // Toggle display based on event type 
+    if (event.type === 'mouseenter') {
+      contentOriginal.style.display = 'none';
+      contentHover.style.display = 'inline';
+    } else if (event.type === 'mouseleave') {
+      contentOriginal.style.display = 'inline';
+      contentHover.style.display = 'none';
+    }
+  }
+}
+
+// Add event listeners only if the elements exist
+if (leftLink) {
+  leftLink.addEventListener('mouseenter', (event) => {
+    handleLinkHover(event, '.content-original', '.content-hover-previous');
+  });
+
+  leftLink.addEventListener('mouseleave', (event) => {
+    handleLinkHover(event, '.content-original', '.content-hover-previous');
+  });
+}
+
+if (rightLink) {
+  rightLink.addEventListener('mouseenter', (event) => {
+    handleLinkHover(event, '.content-original', '.content-hover-next');
+  });
+
+  rightLink.addEventListener('mouseleave', (event) => {
+    handleLinkHover(event, '.content-original', '.content-hover-next');
+  });
+}
+
+if (centerLink) {
+  centerLink.addEventListener('mouseenter', (event) => {
+    // When hovering on center link, show general hover content 
+    handleLinkHover(event, '.content-original', '.content-hover');
+  });
+
+  centerLink.addEventListener('mouseleave', (event) => {
+    // When leaving center link, show original content 
+    handleLinkHover(event, '.content-original', '.content-hover');
+  });
+}
+
+/*---------------------------------------------------------------*/
+
+
+/*---------------------------------------------------------------*/
+// Toc for each chapter
+
+window.addEventListener('scroll', function () {
+  let sections = document.querySelectorAll('div[id^="section"]');
+  let tocLinks = document.querySelectorAll('.toc-chapter a');
+  let scrollPosition = window.scrollY;
+
+  sections.forEach((section, index) => {
+    let sectionTop = section.offsetTop - 100; // Adjust as needed
+    let sectionBottom = sectionTop + section.clientHeight;
+
+    if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+      tocLinks.forEach((link) => {
+        //link.classList.remove('active');
+        link.classList.remove('highlight');
+      });
+      //tocLinks[index].classList.add('active');
+      tocLinks[index].classList.add('highlight');
+    }
+  });
+});
+
+/*---------------------------------------------------------------*/
+
+
+/*---------------------------------------------------------------*/
+/* 
+ I need the following code to show 
+ modal for the "support message"
+*/
+// Get the modal
+let modal = document.getElementById("myModal");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+//btn.onclick = function() {
+//  modal.style.display = "block";
+//}
+
+function showMessage() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+  clearInterval(myInterval);
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    clearInterval(myInterval);
+  }
+
+}
+
+let myInterval = setInterval(function () {
+  showMessage();
+}, 1800000);
+// 3000 3 seconds
+// 8000 8 seconds
+// 30000 30 seconds
+// 60000 1 min
+// 120000 2 min
+// 300000 5 mins
+// 600000 10 mins
+// 1200000 20 mins
+// 1800000 30 mins
+
+/*---------------------------------------------------------------*/
+
+
+/*--------------- Stop scrolling on iframes ---------------------*/
+var iframeContainers = document.querySelectorAll('.iframeContainer');
+
+// Add event listeners for mouse entering and leaving each iframe
+iframeContainers.forEach(function (container) {
+  container.addEventListener('mouseenter', function () {
+    // Hide overflow of the body when mouse enters iframe area
+    document.body.style.overflow = 'hidden';
+  });
+
+  container.addEventListener('mouseleave', function () {
+    // Show overflow of the body when mouse leaves iframe area
+    document.body.style.overflow = 'auto';
+  });
+});
+/*---------------------------------------------------------------*/
+
+
+/*---------------------------------------------------------------*/
+/* 
+  The follwing function is to include an HTML file in the slides
+  Source: https://www.w3schools.com/howto/howto_html_include.asp
+*/
+function includeHTML() {
+  let z, i, elmnt, file, xhttp;
+  /*loop through a collection of all HTML elements:*/
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      /*make an HTTP request using the attribute value as the file name:*/
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+          if (this.status == 200) {
+            elmnt.innerHTML = this.responseText;
+          }
+          if (this.status == 404) {
+            elmnt.innerHTML = "Page not found.";
+          }
+          /*remove the attribute, and call this function once more:*/
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+        }
+      }
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /*exit the function:*/
+      return;
+    }
+  }
+};
+
+// Finally call this funciton to include HTML
+includeHTML();
+
+/*---------------------------------------------------------------*/
